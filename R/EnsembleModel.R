a98ccda47f20ea92f89eeb1f797350a4d53dbdba
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-04-18 23:47:23 -0400

    update package w ensemble model

--- R/EnsembleModel.R
+++ R/EnsembleModel.R
@@ -1,43 +1,23 @@
-require(tidyverse)
-require(jsonlite)
-require(httr)
-require(lubridate)
-require(DT)
+#' @title Creates an Ensemble Prediction Model
+#'
+#' @description This package creates a complex prediction model based on team identifiers and previous game results. This prediction model is build on a foundation of many other prediction models.
+#'
+#' @param gameIds
+#'
+#' @param homeTeamIds
+#'
+#' @param awayTeamIds
+#'
+#' @param homeScores
+#'
+#' @param awayScores
+#'
+#' @return named base prediction models and ensemble prediction function
+#'
+#' @examples
+#'
+#' @export
 
-source('R/SetupGames.R')
-source('R/BradleyTerry.R')
-source('R/TeamOlsRating.R')
-source('R/GameScoresStdDev.R')
-source('R/ZScoreDeviation.R')
-source('R/PowerRankPoints.R')
-
-get_api_football_json_from_url <- function(url){
-	key <- '71c766b005msh1ddcb3052482d45p14fb6cjsnaf3ff7900508'
-	headers <- c(key)
-	names(headers) <- 'X-RapidAPI-Key'
-
-	response <- httr::GET(url, add_headers(.headers = headers))
-	rawJson <- httr::content(response, as = 'text')
-	json <- jsonlite::fromJSON(rawJson)$api
-	return (json)
-}
-
-get_games_by_league_id <- function(leagueId){
-	url <- paste0('https://api-football-v1.p.rapidapi.com/v2/fixtures/league/', leagueId)
-	json <- get_api_football_json_from_url(url)
-	games <- json$fixtures
-	return (games)
-}
-
-leagueId <- 785
-rawGames <- get_games_by_league_id(leagueId)
-
-gameIds <- rawGames$fixture_id
-homeTeamIds <- rawGames$homeTeam$team_name
-awayTeamIds <- rawGames$awayTeam$team_name
-homeScores <- rawGames$goalsHomeTeam
-awayScores <- rawGames$goalsAwayTeam
-isNeutralSite <- rep(x = F, times = length(gameIds))
 
 ensemble_model <- function(gameIds, homeTeamIds, awayTeamIds, homeScores, awayScores, isNeutralSite){
 	g <- setup_games(gameIds, homeTeamIds, awayTeamIds, homeScores, awayScores, isNeutralSite)
